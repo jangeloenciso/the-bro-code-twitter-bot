d48fa7ec1c344ed253ac2e3400bf1befb5222650
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2020-09-30 05:43:53 +0800

    Removed keys and added readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+
+This is a Twitter bot inspired by the popular sitcom How I Met Your Mother.
+
+How to use:
+
+tag @TheBroCodeBot in your tweet and add a number from 1-150. Every number corresponds to one article in The Bro Code.
+
+ENJOY! IT'S GOING TO BE LEGEN - WAIT FOR IT - DARY! LEGENDARY!
--- keys.py
+++
@@ -1,8 +0,0 @@
-
-
-keys = dict(
-	consumer_key =  "ZDVW6rhnGK3kU2S5RnQYJPjct",
-	consumer_secret =       "ncudsTOSa7XhFrrJ4TGBfczb119VZOKd5565IC11nQHGNlVGAc",
-	access_token =          "1305620177699004416-Ff4yBqnVum3O2zD7ZRfDHmFiK9M4NI",
-	access_token_secret =   "itvJjEo41J0G6oWpzfBNSCwPhLlpHeoDaNtKOK5w0PqKT",
-)
