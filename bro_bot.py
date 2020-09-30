896db7f62a0f57fc5f6c7dbc7462c057fe320b2f
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2020-09-30 11:58:40 +0800

    With keys and fixed algorithm

--- bro_bot.py
+++ bro_bot.py
@@ -13,28 +13,47 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-
 mentions = api.mentions_timeline()
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
 
 i = 0
 
 def reply_to_tweets():
-    for mention in mentions:
+    print('retrieving and replying to tweets...', flush=True)
+    # DEV NOTE: use 1060651988453654528 for testing.
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    # NOTE: We need to use tweet_mode='extended' below to show
+    # all full tweets (with full_text). Without it, long tweets
+    # would be cut off.
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
         for i in reversed(range(151)):
-            if str(i) in mention.text:
-                api.update_status('@' + mention.user.screen_name + " " + articles[i], mention.id)
-                break
+            if str(i) in mention.full_text:
+                try:
+                    api.update_status('@' + mention.user.screen_name + " " + articles[i], mention.id)
+                    break
+                except tweepy.TweepError:
+                    print("error")
+                
 
 while True:
-    print('Running...')
     reply_to_tweets()
     time.sleep(15)
  
-
-# for mention in mentions:
-    # for mention in reversed(mentions):
-    #     if 'code' in mention.full_text.lower():
-    #         print('found #helloworld!', flush=True)
-    #         print('responding back...', flush=True)
-    #         api.update_status('@' + mention.user.screen_name +
-    #                 'BrosBeforeHoes', mention.id)
